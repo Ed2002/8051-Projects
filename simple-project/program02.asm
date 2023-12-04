0b19ce2c0cb069f252b438e9b43ca4ba68122707
Ed2002 <ed49516@gmail.com>
2023-12-04 19:31:31 -0300

    Melhorias

--- simple-project/program02.asm
+++ simple-project/program02.asm
@@ -1,8 +1,9 @@
 ORG 0 ;Start
 
-MOV A, #0D
+MOV A, #0D ;MOVE 0 TO @A
 
-MAIN_LOOP:
-	INC A
-	CJNE A, #9D, MAIN_LOOP
+MAIN_LOOP: ;MAIN_LOOP function
+	INC A ;Increment @A value
+	CJNE A, #9D, MAIN_LOOP ;Conpare and Jump if @A is not equal to 9 jump to MAIN_LOOP function
+
 END ;End
---
+++ simple-project/program03.asm~
@@ -0,0 +1,7 @@
+ORG 0  ;Start
+MOV  P0, #1  ;MOVE 1 TO @P0
+MOV  P1, #2  ;MOVE 2 TO @P1
+MOV  A, P0   ;MOVE @A value TO @P0
+MOV  P0, P1  ;MOVE @P0 value TO @P1
+MOV  P1, A   ;MOVE @A value TO @P1
+END  ;End

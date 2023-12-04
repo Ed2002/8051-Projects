63ab42477ba8dc04b4eb7bb086b626052fc0bf97
Ed2002 <ed49516@gmail.com>
2023-12-04 02:17:42 -0300

    Comments

--- simple-project/program01.asm
+++ simple-project/program01.asm
@@ -1,6 +1,6 @@
-ORG 0
-MOV A, #10H
-MOV R0, #30H
-ADD A, R0
-MOV R1, A
-END
+ORG 0 ;Start
+MOV A, #10H ;Move 10 to @A register
+MOV R0, #30H ;Move 30 to @R0 register
+ADD A, R0 ;Add to @A register value @R0 register value
+MOV R1, A ;Move @A register value to @R0 register
+END ;End
